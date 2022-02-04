866f0a4e9d4c9f5ffb1a0758e6bd99cbd4f823fb
Haochen-Luo <1571767513@qq.com>
2022-02-04 21:41:43 +0000

    Modify the starting

--- src/main/java/com/example/ebook/EbookApplication.java
+++ src/main/java/com/example/ebook/EbookApplication.java
@@ -1,13 +1,26 @@
 package com.example.ebook;
 
+import org.apache.juli.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EbookApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookApplication.class);
+
     public static void main(String[] args) {
+        System.out.println("running");
         SpringApplication.run(EbookApplication.class, args);
+
+        SpringApplication app = new SpringApplication(EbookApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("successfully started!");
+
     }
 
 }
