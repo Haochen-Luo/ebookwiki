1f82bc2c367f21fe20c96cb486575844eb3042f5
Haochen-Luo <1571767513@qq.com>
2022-02-09 21:07:46 +0000

    Fix the bug caused by @ComponentScan in EbookApplication.java

---
+++ src/main/java/com/example/ebook/config/EbookApplication.java
@@ -0,0 +1,32 @@
+package com.example.ebook.config;
+
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// @ComponentScan({"com.jiawa", "com.test"})
+@ComponentScan("com.example")
+@SpringBootApplication
+@MapperScan("com.example.ebook.mapper")
+//@EnableScheduling
+//@EnableAsync
+public class EbookApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(EbookApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
